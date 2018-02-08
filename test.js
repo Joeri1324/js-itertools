05289a7f66d4a2b197f41dc79e3a116d01f8502a
joeri1324 <joeri.sleegers@student.uva.nl>
2018-02-08 21:15:06 +0100

    added tests

--- test.js
+++ test.js
@@ -1,5 +1,33 @@
 import * as iter from './lib/index.js';
+var assert = require('assert');
 
 
-// Range
-console.log([1, 2, 3, 5] === iter.list(iter.range(1, 4)));
+test('Range works', () => {
+    expect([1, 2, 3, 4]).toEqual(iter.list(iter.range(1, 5)));
+});
+
+test('Range works with descending stepsize', () => {
+    expect([5, 4, 3, 2]).toEqual(iter.list(iter.range(5, 1, -1)));
+});
+
+test('Range return empty array with out of bounds range', () => {
+    expect([]).toEqual(iter.list(iter.range(1, 5, -1)));
+});
+
+test('Accumelate works', () => {
+    expect([1, 3, 6]).toEqual(iter.list(iter.accumulate(iter.range(1, 4))));
+});
+
+test('Takewhile works', () => {
+    expect([1, 2, 3, 4])
+        .toEqual(
+            iter.list(
+                iter.takeWhile((x) => x < 5, 
+                               iter.range(1, 10))
+            )
+        );
+});
+
+for(let elem of iter.zip([iter.range(1, 5), iter.range(1, 5)])) {
+    console.log(elem);
+}
